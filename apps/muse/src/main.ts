f18a0c8efca3d55f80ac4fa558a679dafb3900a5
Jurien Hamaker <whoami@jurien.dev>
2023-05-27 22:06:44 +0200

    fix: Do not build using nest

--- apps/muse/src/main.ts
+++ apps/muse/src/main.ts
@@ -10,15 +10,17 @@ async function bootstrap() {
 	});
 	app.setGlobalPrefix('api');
 
-	const config = new DocumentBuilder()
-		.setTitle('Muse API')
-		.setDescription("The muse API for it's dashboard")
-		.setVersion('1.0')
-		.addTag('muse')
-		.addBearerAuth()
-		.build();
-	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('swagger', app, document);
+	if (process.env.NODE_ENV !== 'production') {
+		const config = new DocumentBuilder()
+			.setTitle('Muse API')
+			.setDescription("The muse API for it's dashboard")
+			.setVersion('1.0')
+			.addTag('muse')
+			.addBearerAuth()
+			.build();
+		const document = SwaggerModule.createDocument(app, config);
+		SwaggerModule.setup('swagger', app, document);
+	}
 
 	await app.listen(3000);
 }
