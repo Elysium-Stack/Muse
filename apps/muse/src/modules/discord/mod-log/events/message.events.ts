a6266840d84712dee009944b8244e64e7bd60e9f
Jurien Hamaker <whoami@jurien.dev>
2023-06-18 20:11:31 +0200

    fix(Muse/Mod Log/Messages): Do some chunking on sizes

--- apps/muse/src/modules/discord/mod-log/events/message.events.ts
+++ apps/muse/src/modules/discord/mod-log/events/message.events.ts
@@ -31,47 +31,54 @@ export class ModLogMessageEvents {
 			return;
 		}
 
-		const embed = new EmbedBuilder()
-			.setTitle(`Message delete in ${channel.name}`)
-			.setDescription(
-				`https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`,
-			)
-			.addFields(
-				{
-					name: 'Content',
-					value: message.content,
-				},
-				...(message.attachments.size
-					? [
-							{
-								name: 'Attachments',
-								value: message.attachments
-									.map((a) => a.url)
-									.join('\n'),
-							},
-					  ]
-					: []),
-			)
-			.setAuthor({
-				name: `${message.author.username}${
-					message.author.discriminator === '0'
-						? ''
-						: `#${message.author.discriminator}`
-				} | ${message.author.id}`,
-				iconURL: message.author.displayAvatarURL() || undefined,
-			})
-			.setColor(EMBED_STATUS_COLORS.danger)
-			.setTimestamp();
+		try {
+			const content = message.content.match(/(.|[\r\n]){1,1024}/g);
 
-		const deleteChannel = await message.guild.channels.fetch(
-			deleteChannelId,
-		);
+			const embed = new EmbedBuilder()
+				.setTitle(`Message delete in ${channel.name}`)
+				.setDescription(
+					`https://discord.com/channels/${message.guildId}/${message.channelId}/${message.id}`,
+				)
+				.addFields(
+					...content.map((part, i) => ({
+						name: i === 0 ? 'Content' : ' ',
+						value: part,
+					})),
+					...(message.attachments.size
+						? [
+								{
+									name: 'Attachments',
+									value: message.attachments
+										.map((a) => a.url)
+										.join('\n'),
+								},
+						  ]
+						: []),
+				)
+				.setAuthor({
+					name: `${message.author.username}${
+						message.author.discriminator === '0'
+							? ''
+							: `#${message.author.discriminator}`
+					} | ${message.author.id}`,
+					iconURL: message.author.displayAvatarURL() || undefined,
+				})
+				.setColor(EMBED_STATUS_COLORS.danger)
+				.setTimestamp();
 
-		if (!deleteChannel.isTextBased()) {
-			return;
+			const deleteChannel = await message.guild.channels.fetch(
+				deleteChannelId,
+			);
+
+			if (!deleteChannel.isTextBased()) {
+				return;
+			}
+
+			await deleteChannel.send({ embeds: [embed] });
+		} catch (err) {
+			console.log(err);
+			this._logger.error(err);
 		}
-
-		await deleteChannel.send({ embeds: [embed] });
 	}
 
 	@On(Events.MessageUpdate)
@@ -99,59 +106,146 @@ export class ModLogMessageEvents {
 		if (!(channel instanceof TextChannel)) {
 			return;
 		}
+		try {
+			const originalContent =
+				original.content.match(/(.|[\r\n]){1,1024}/g);
+			const updatedContent =
+				original.content.match(/(.|[\r\n]){1,1024}/g);
 
-		const embed = new EmbedBuilder()
-			.setTitle(`Message edited in ${channel.name}`)
-			.setDescription(
-				`https://discord.com/channels/${updated.guildId}/${updated.channelId}/${updated.id}`,
-			)
-			.addFields(
-				{
-					name: 'Original content',
-					value: original.content,
-				},
-				{
-					name: 'Edited content',
-					value: updated.content,
-				},
-				...(original.attachments.size
-					? [
-							{
-								name: 'Original attachments',
-								value: original.attachments
-									.map((a) => a.url)
-									.join('\n'),
-							},
-					  ]
-					: []),
-				...(updated.attachments.size
-					? [
-							{
-								name: 'Edited attachments',
-								value: updated.attachments
-									.map((a) => a.url)
-									.join('\n'),
-							},
-					  ]
-					: []),
-			)
-			.setAuthor({
-				name: `${original.author.username}${
-					original.author.discriminator === '0'
-						? ''
-						: `#${original.author.discriminator}`
-				} | ${original.author.id}`,
-				iconURL: original.author.displayAvatarURL() || undefined,
-			})
-			.setColor(EMBED_STATUS_COLORS.warning)
-			.setTimestamp();
+			const embeds = [];
 
-		const editChannel = await original.guild.channels.fetch(editChannelId);
+			const embed = new EmbedBuilder()
+				.setTitle(`Message edited in ${channel.name}`)
+				.setDescription(
+					`https://discord.com/channels/${updated.guildId}/${updated.channelId}/${updated.id}`,
+				)
+				.addFields(
+					...(originalContent?.length === 1 &&
+					updatedContent?.length === 1
+						? [
+								{
+									name: 'Original content',
+									value: originalContent[0],
+								},
+						  ]
+						: []),
+					...(originalContent?.length === 1 &&
+					updatedContent?.length === 1
+						? [
+								{
+									name: 'Edited content',
+									value: updatedContent[0],
+								},
+						  ]
+						: []),
+					...(original.attachments.size
+						? [
+								{
+									name: 'Original attachments',
+									value: original.attachments
+										.map((a) => a.url)
+										.join('\n'),
+								},
+						  ]
+						: []),
+					...(updated.attachments.size
+						? [
+								{
+									name: 'Edited attachments',
+									value: updated.attachments
+										.map((a) => a.url)
+										.join('\n'),
+								},
+						  ]
+						: []),
+				)
+				.setAuthor({
+					name: `${original.author.username}${
+						original.author.discriminator === '0'
+							? ''
+							: `#${original.author.discriminator}`
+					} | ${original.author.id}`,
+					iconURL: original.author.displayAvatarURL() || undefined,
+				})
+				.setColor(EMBED_STATUS_COLORS.warning)
+				.setTimestamp();
 
-		if (!editChannel.isTextBased()) {
-			return;
+			embeds.push(embed);
+
+			if (originalContent.length > 1) {
+				embeds.push(
+					new EmbedBuilder()
+						.setTitle(`Original content`)
+						.setDescription(
+							`https://discord.com/channels/${original.guildId}/${
+								original.channelId
+							}/${original.id}\n
+							${originalContent.join('')}`,
+						)
+						// .addFields(
+						// 	...originalContent.map((part, i) => ({
+						// 		name: ' ',
+						// 		value: part,
+						// 	})),
+						// )
+						.setAuthor({
+							name: `${original.author.username}${
+								original.author.discriminator === '0'
+									? ''
+									: `#${original.author.discriminator}`
+							} | ${original.author.id}`,
+							iconURL:
+								original.author.displayAvatarURL() || undefined,
+						})
+						.setColor(EMBED_STATUS_COLORS.warning)
+						.setTimestamp(),
+				);
+			}
+
+			if (updatedContent.length > 1) {
+				embeds.push(
+					new EmbedBuilder()
+						.setTitle(`Edited content`)
+						.setDescription(
+							`https://discord.com/channels/${updated.guildId}/${
+								updated.channelId
+							}/${updated.id}\n
+							${updatedContent.join('')}`,
+						)
+						// .addFields(
+						// 	...updatedContent.map((part, i) => ({
+						// 		name: ' ',
+						// 		value: part,
+						// 	})),
+						// )
+						.setAuthor({
+							name: `${updated.author.username}${
+								updated.author.discriminator === '0'
+									? ''
+									: `#${updated.author.discriminator}`
+							} | ${updated.author.id}`,
+							iconURL:
+								updated.author.displayAvatarURL() || undefined,
+						})
+						.setColor(EMBED_STATUS_COLORS.warning)
+						.setTimestamp(),
+				);
+			}
+
+			const editChannel = await original.guild.channels.fetch(
+				editChannelId,
+			);
+
+			if (!editChannel.isTextBased()) {
+				return;
+			}
+
+			for (let embed of embeds) {
+				await editChannel.send({ embeds: [embed] });
+			}
+		} catch (err) {
+			console.log(err);
+			this._logger.error(err);
 		}
-
-		await editChannel.send({ embeds: [embed] });
 	}
 }
