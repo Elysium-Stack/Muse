307d90fde60ac6df02854618109ce1666606adb7
Jurien Hamaker <whoami@jurien.dev>
2024-09-13 20:07:18 +0200

    chore: add base prettier & lint setup

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+var conventions = require('./node_modules/@paperless/conventions/.prettierrc');
+
+module.exports = Object.assign(conventions, {
+	// tailwindConfig: './tailwind.config.ts',
+  plugins: []
+});
